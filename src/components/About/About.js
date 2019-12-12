2816fecfe1558c573462ad56950c24d94adff8cc
Nathan Chau <nathankcchau@gmail.com>
2019-12-11 20:09:35 -0500

    Added about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,7 +4,16 @@ import './About.css';
 
 const about = props => (
     <div id="about">
-
+        <div className="container-about">
+            <div className="about-title">
+                About me
+            </div>
+            <div className="about-text">
+                I have a passion for dessigning and creating beautiful things and I love what I do!
+                As a developer, my goal is to combine features to create the best experiencing product for users. 
+                As a musician, I like to create music that people would enjoy while also helping others.
+            </div>
+        </div>
     </div>
 );
 
