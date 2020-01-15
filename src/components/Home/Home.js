453b5e47cf391ece249fd09d8cfea839f629f9ee
Nathan Chau <nathankcchau@gmail.com>
2020-01-14 21:07:43 -0500

    Added Youtube button

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 
 import './Home.css';
 import BrandIcon from '../Icon/BrandIcon';
-import { faLinkedin ,faGithubSquare } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin ,faGithubSquare, faYoutubeSquare } from '@fortawesome/free-brands-svg-icons';
 import { faFile } from '@fortawesome/free-solid-svg-icons';
 
 import resume from '../../Files/Resume.pdf';
@@ -23,6 +23,7 @@ class main extends Component {
                         <BrandIcon hyperLink="https://www.linkedin.com/in/nathankcchau/" iconClass={faLinkedin}/>
                         <BrandIcon hyperLink="https://github.com/leiffen" iconClass={faGithubSquare}/>
                         <BrandIcon hyperLink= {resume} iconClass={faFile}/>
+                        <BrandIcon hyperLink="https://www.youtube.com/channel/UCvzsdNHcJSWwZR_MRH6uVsQ" iconClass={faYoutubeSquare}/>
                     </div>
                 </div>
             </div>
